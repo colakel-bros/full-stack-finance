044ce061c92e8431580f3cceeda0141405907385
serifcolakel <serifcolakel0@gmail.com>
2024-02-24 15:58:49 +0300

    Add npmrc, tsconfig.json, and global.css files for shadcn ui for web

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+const baseConfig = require('../../tailwind.config');
+
+const relativePathToRemoteApplication = join(
+  __dirname,
+  'apps/web',
+);
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    ...baseConfig?.content || [],
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}',
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+    ...createGlobPatternsForDependencies(relativePathToRemoteApplication),
+  ],
+  ...baseConfig,
+};
